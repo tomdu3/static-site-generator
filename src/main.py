ccf94d1411ba3320d72a7a5acd8509e82bbf5fe1
Tomislav Dukez <tomdu3@ymail.com>
2026-01-05 17:09:48 +0000

    add textnode and main

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnote import TextNode
+
+def main():
+    test_node = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()
+
+print('hello world')
---
+++ src/textnote.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Bender(Enum):
+    AIR_BENDER = "air"
+    WATER_BENDER = "water"
+    EARTH_BENDER = "earth"
+    FIRE_BENDER = "fire"
+
+class TextNode():
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode(self.text, self.text_type, self.url)"
+
